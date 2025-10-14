ddde630e50db662ff4cf6134e33b88b4ea45b037
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-10-14 16:04:20 +0530

    created course models

---
+++ server/models/course.models.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    trim: true,
+  },
+  courseDescription: {
+    type: String,
+    trim: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  whatYouWillLearn: {
+    type: String,
+    trim: true,
+  },
+  courseContent: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Section",
+    },
+  ],
+  ratingAndReview: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "RatingAndReview",
+  },
+  price: {
+    type: Number,
+  },
+  thumbnail: {
+    type: String,
+  },
+  tag: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Tag",
+  },
+  studentsEnrolled: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  ],
+});
+
+export default mongoose.model("Course", courseSchema);

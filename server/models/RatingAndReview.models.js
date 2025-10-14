521e44e15149bd0ead36977b8cb95c513eae0a35
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-10-14 16:09:19 +0530

    craeted rating and review model

---
+++ server/models/RatingAndReview.models.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const ratingAndReviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("RatingAndReview", ratingAndReviewSchema);

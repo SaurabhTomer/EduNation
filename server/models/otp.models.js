4ac4642c308ea031665ca8e246e9a16a3c1d437a
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-10-14 16:39:59 +0530

    generate otpEmail using pre middleware hook

--- server/models/otp.models.js
+++ server/models/otp.models.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import { mailSender } from "../utils/mailSender.js";
 
 const otpSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    trim:true,
   },
   otp: {
     type: String,
@@ -16,4 +18,22 @@ const otpSchema = new mongoose.Schema({
   },
 });
 
-export default mongoose.model("Otp" , otpSchema)
+
+// function to send email
+async function sendVerificationEmail(email , otp){
+    try {
+        const mailResponse = await mailSender(email , "Verification Email from EduNation" , otp);
+        console.log("Email send successfully");
+    } catch (error) {
+        console.log( "error occured while sending email : ",error.message);
+        throw error;
+    }
+}
+
+// pre middleware (pre hook ) to send otp mail to save user in db and calling next 
+otpSchema.pre("save" , async function(next){
+    await sendVerificationEmail(this.email , this.otp);
+    next();
+})
+
+export default mongoose.model("OTP" , otpSchema)
